1a81bf02c837ec5b0b86fd7084fbb4a8ee521961
tlau425 <tlau425@yahoo.com>
2023-05-02 19:57:46 -0400

    removed carousel, changed color theme

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,18 +1,17 @@
 import './Hero.scss'
-import ProfilePic from '../../assets/images/profile-pic.png'
-
 
 function Hero() {
     return (
         <section className="hero">
             <div className="hero__container">
                 <div className="hero__left">
-                    <h1>Hello My name is Tommy Lau</h1>
-                    <h2>I am a full stack developer</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                    <h1 className="hero__title">Hello, my name is Tommy.</h1>
+                    <h2 className="hero__sub-header">I am a full stack developer</h2>
+                    <p className="hero__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 <div className="hero__right">
-                    <img src={ProfilePic} className="hero__avatar" />
+                    <div className="hero__avatar">
+                    </div>
                 </div>
             </div>
         </section>
