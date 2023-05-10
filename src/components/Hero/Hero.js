4e00c04ca0a727b942f7538f3f30254c5c35e324
tlau425 <tlau425@yahoo.com>
2023-05-10 17:42:09 -0400

    add created projects

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,7 +6,8 @@ function Hero() {
             <div className="hero__container">
                 <div className="hero__left">
                     <h1 className="hero__title">Hello, my name is Tommy.</h1>
-                    <h2 className="hero__sub-header">I am a full stack developer</h2>
+                    
+                    <h2 className="hero__sub-header">Full Stack Developer</h2>
                     <p className="hero__description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                 </div>
                 <div className="hero__right">
